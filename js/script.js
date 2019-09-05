61fc575a77ec11b3c18d53fefd5f56dceba1dc9d
BuildTools <kabirdesarkar@gmail.com>
2019-09-05 08:33:31 +0530

    Dude, the script wasn't even linked

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 console.log("Test")
 $(function(){
+
   $.ajax({
     type:"GET",
     url:"https://pledge-counter.herokuapp.com/api/main/getCount",
     success:function(data){
       $(".pledge-container").css("display","block")
         $(".pledge-counter").html(Number(data)+100)
+        console.log(data)
     },
     error:function(data){
         console.log(data)
